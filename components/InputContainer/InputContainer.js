7f3742d9a70752fe43869ccd69f36a260d4c1cc2
ABSanthosh <a.b.santhosh02@gmail.com>
2023-03-31 19:19:49 +0530

    feat: input box

--- components/InputContainer/InputContainer.js
+++ components/InputContainer/InputContainer.js
@@ -4,8 +4,6 @@ import "./InputContainer.scss";
 import IndianData from "../../utils/india.json";
 import Select from "react-select";
 import * as Accordion from "@radix-ui/react-accordion";
-import { ChevronDownIcon } from '@radix-ui/react-icons';
-
 
 function InputContainer({ data, setData }) {
   const [flightData] = useState(IndianData);
@@ -13,19 +11,6 @@ function InputContainer({ data, setData }) {
 
   return (
     <div className="InputContainer">
-      <Accordion.Root
-        className="AccordionRoot "
-        type="single"
-        defaultValue="item-1"
-        collapsible
-      >
-        <Accordion.Item className="AccordionItem" value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </Accordion.Item>
-      </Accordion.Root>
       <h2>Build Itinerary</h2>
       <hr />
       <div className="InputContainer__inputs">
@@ -163,32 +148,5 @@ function InputContainer({ data, setData }) {
     </div>
   );
 }
-
-const AccordionTrigger = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Header className="AccordionHeader">
-      <Accordion.Trigger
-        className={"AccordionTrigger"}
-        {...props}
-        ref={forwardedRef}
-      >
-        {children}
-        <ChevronDownIcon className="AccordionChevron" aria-hidden />
-      </Accordion.Trigger>
-    </Accordion.Header>
-  )
-);
-
-const AccordionContent = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Content
-      className={"AccordionContent"}
-      {...props}
-      ref={forwardedRef}
-    >
-      <div className="AccordionContentText">{children}</div>
-    </Accordion.Content>
-  )
-);
 
 export default InputContainer;
